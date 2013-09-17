ff79b1475e7a937502efd8779c417dafe3daa697
Corey Donohoe <atmos@atmos.org>
2013-09-17 14:25:36 -0700

    fix up the responses we actually get back from 37s

--- lib/campfiyah/user.rb
+++ lib/campfiyah/user.rb
@@ -1,12 +1,12 @@
 module Campfiyah
   class User
-    attr_accessor :updated_at, :id, :name, :email
-    def initialize(adapter, id, name, email, updated_at)
-      @adapter, @id, @name, @email, @updated_at = adapter, id, name, email, updated_at
+    attr_accessor :id, :name, :email
+    def initialize(adapter, id, name, email)
+      @adapter, @id, @name, @email = adapter, id, name, email
     end
 
     def self.from_hash(hash, adapter)
-      new(adapter, hash["id"], hash["name"], hash["email"], hash["updated_at"])
+      new(adapter, hash["id"], hash["name"], hash["email_address"])
     end
   end
 end
